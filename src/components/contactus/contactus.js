8c082375e388e5c231d0c513cff3b846924ed349
Thủy Đào Duy <daoduythuy@gmail.com>
2019-09-16 23:50:29 +0700

    update code

---
+++ src/components/contactus/contactus.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me for any work or suggestions below
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>Email | daoduythuy@gmail.com</h4>
+                <h4>Phone | 0375365123</h4>
+                <h4>Skype | daoduythuy9690</h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,187 @@
+let resumeData = {
+  "imagebaseurl":"https://thuydao.github.io/cv",
+  "name": "Thuy Dao",
+  "role": "Mobile Technical Leader",
+  "linkedinId":"Thủy Đào",
+  "skypeid": "daoduythuy9690",
+  "roleDescription": " I started working in early 2012. I can work with mobile native (iOS) and hybrid using React Native.\
+   I have exp about e-commerce, social networking, xmpp, pjsip, socket, chart, media...\
+   I am willing to learn and be to new challenges in the future",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/thủy-đào-3452a840/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/thuydao",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"skype://daoduythuy9690?chat",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I have finished my B.E. in Computer Science \n \
+   and have been working in industry for past few years.",
+  "address":"Ha Noi, Viet Nam",
+  "website":"https://www.facebook.com/daoduythuy",
+  "education":[
+    {
+      "UniversityName":"Post & Telecommunications Institute of Technology, Viet Nam",
+      "specialization":"Computer Science",
+      "MonthOfPassing":"Jun",
+      "YearOfPassing":"2016",
+      "Achievements":"Master"
+    },
+    {
+      "UniversityName":"Post & Telecommunications Institute of Technology, Viet Nam",
+      "specialization":"Software Technology | Infomation Technology",
+      "MonthOfPassing":"Dec",
+      "YearOfPassing":"2012",
+      "Achievements":"Engineer"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"VTVCab",
+      "specialization":"Tech Leader",
+      "MonthOfLeaving":"-",
+      "YearOfLeaving":"Present",
+      "Achievements":"- Technology research\
+      \n- Development VTVcab ON.\
+      \n- Development DRM, integration to Harmonic transcode system.\
+      \n- Management ONSport team\
+      "
+    },
+    {
+      "CompanyName":"SmartOSC",
+      "specialization":"Mobile Tech Leader",
+      "MonthOfLeaving":"May",
+      "YearOfLeaving":"2017",
+      "Achievements":"- Pre-sale.\
+      \n- Define Architecture, convention for SmartOSC’s projects.\
+      \n- Review code, avoid any risk in all commits of the members and follow convention.\
+      \n- Build library (‘TDCore’), research new trend technology.\
+      \n- Join to dedicated team, project base, product team.\
+      \n- Resource management\
+      "
+    },
+    {
+      "CompanyName":"QSoft VietNam",
+      "specialization":"Mobile Tech Leader",
+      "MonthOfLeaving":"Feb",
+      "YearOfLeaving":"2015",
+      "Achievements":"- Research new technology (Swift, RAD, game …).\
+      \n- Support other ios developer in problem solving and bug fixing.\
+      \n- Incharge of solving difficult technical problems.\
+      \n- Successfully applied 3rd parties library/frame work to qsoft project such as easy tableView, cache resource webview, the google directions  …\
+      \n- Sale support : aswer technical question, prepare technical proposal, estimate project effort.\
+      "
+    },
+    {
+      "CompanyName":"VMODEV",
+      "specialization":"Developer",
+      "MonthOfLeaving":"Dec",
+      "YearOfLeaving":"2013",
+      "Achievements":"- Development applications chat, voice calls, social networking, and utility applications…\
+      \n- The technologies and languages applied for the job: Xmpp, lync, objective –c, html, javascript, phonegap, java …\
+      \n- Management and responsible for the progress and quality for team.\
+      "
+    },
+  ],
+  "skillsDescription":"Skills list",
+  "skills":[
+    {
+      "skillname":"Objective-C"
+    },
+    {
+      "skillname":"Swift"
+    },
+    {
+      "skillname":"NodeJS"
+    },
+    {
+      "skillname":"React Native"
+    },
+    {
+      "skillname":"ReactJS"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"VTVcab ON",
+      "description":"Media",
+      "imgurl":"images/portfolio/cabon.png",
+      "link":"https://apps.apple.com/vn/app/vtvcab-on/id1068120150"
+    },
+    {
+      "name":"ON Sports",
+      "description":"Media",
+      "imgurl":"images/portfolio/onsport.png",
+      "link":"https://apps.apple.com/vn/app/on-sports/id1282845933"
+    },
+    {
+      "name":"My VNPost",
+      "description":"VTVcab",
+      "imgurl":"images/portfolio/vnpost.png",
+      "link":"https://play.google.com/store/apps/details?id=vn.vnpost.myvnpost&hl=en"
+    },
+    {
+      "name":"X-POS",
+      "description":"Ecommerce",
+      "imgurl":"images/portfolio/xpos.jpg",
+      "link":"https://apps.apple.com/us/app/x-pos/id893838651"
+    },
+    {
+      "name":"eWAY SmartPOS",
+      "description":"Ecommerce",
+      "imgurl":"images/portfolio/eway.png",
+      "link":"https://apps.apple.com/au/app/eway-smartpos/id1029746660"
+    },
+    {
+      "name":"PopBook",
+      "description":"Ecommerce",
+      "imgurl":"images/portfolio/popbook.png",
+      "link":"https://apps.apple.com/gb/app/pop-book-photobook-cutest/id946922333"
+    },
+    {
+      "name":"Brewery Explorer",
+      "description":"Social",
+      "imgurl":"images/portfolio/Brewery.jpg",
+      "link":"https://apps.apple.com/us/app/brewery-explorer/id943707682"
+    },
+    {
+      "name":"SimiCart",
+      "description":"Ecommerce",
+      "imgurl":"images/portfolio/SimiCart.jpg",
+      "link":"https://apps.apple.com/us/app/simi-superior/id1045850464"
+    },
+    {
+      "name":"Paktor",
+      "description":"Social",
+      "imgurl":"images/portfolio/paktor.jpg",
+      "link":"https://apps.apple.com/us/app/paktor-dating-match-and-meet-singles-to-mingle/id654972611"
+    },
+    {
+      "name":"Tehuan",
+      "description":"Social",
+      "imgurl":"images/portfolio/tehuan.jpg",
+      "link":"https://play.google.com/store/apps/details?id=vn.vnpost.myvnpost&hl=en"
+    },
+  ],
+  "testimonials":[
+    {
+      "description":"",
+      "name":"eWay Customer"
+    },
+    // {
+    //   "description":"This is a sample testimonial",
+    //   "name":"Some technical guy"
+    // }
+  ]
+}
+
+export default resumeData
