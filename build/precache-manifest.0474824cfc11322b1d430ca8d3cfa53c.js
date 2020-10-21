5b3e122e1710ae47b4e446e4129edc07381560cf
Thuy <thuy@axlehire.com>
2020-10-21 14:56:51 +0700

    update axlehire job

---
+++ build/precache-manifest.0474824cfc11322b1d430ca8d3cfa53c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcaa82d43b31eb072cfa9e9cbcf4705",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "411430c0f80d66bcc1e1",
+    "url": "/cv/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "704f518e4a2ae3b0e930",
+    "url": "/cv/static/js/2.428394aa.chunk.js"
+  },
+  {
+    "revision": "411430c0f80d66bcc1e1",
+    "url": "/cv/static/js/main.f5da4c8f.chunk.js"
+  },
+  {
+    "revision": "f239d40d56cbb030f1df",
+    "url": "/cv/static/js/runtime~main.6e6e4965.js"
+  }
+]);
